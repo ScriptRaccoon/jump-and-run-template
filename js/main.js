00cfa00620c34e3e045d10b4e9b6dcace133ca51
Script Raccoon <javascript.martin@gmail.com>
2021-01-04 02:21:51 +0100

    remove red dots

--- js/main.js
+++ js/main.js
@@ -13,11 +13,6 @@ new Rectangle({ pos: [700, 900], size: [300, 10], color: "blue" });
 new Rectangle({ pos: [100, 300], size: [400, 10], color: "blue" });
 new Rectangle({ pos: [700, 300], size: [200, 10], color: "blue" });
 
-new Rectangle({ pos: [990, 0], size: [10, 10], color: "red" });
-new Rectangle({ pos: [0, 0], size: [10, 10], color: "red" });
-new Rectangle({ pos: [0, 990], size: [10, 10], color: "red" });
-new Rectangle({ pos: [990, 990], size: [10, 10], color: "red" });
-
 new Box({ pos: [250, 520], size: [50, 80], color: "orange" });
 new Box({ pos: [250, 230], size: [50, 70], color: "teal" });
 new Box({ pos: [720, 700], size: [100, 100], color: "darkgreen" });
