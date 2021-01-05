2723d3bed2658b31131de7afa480b56d3427875e
Script Raccoon <javascript.martin@gmail.com>
2021-01-05 22:49:55 +0100

    level 0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-import { level1 } from "./levels/level1.js";
+import { level0 } from "./levels/level0.js";
+// import { level1 } from "./levels/level1.js";
 
-level1.drawObjects();
+level0.drawObjects();
