740b61e6352ac4a6810a30f8a1f9e8197b45174f
Script Raccoon <javascript.martin@gmail.com>
2021-01-07 01:29:26 +0100

    small level counter

--- js/Game.js
+++ js/Game.js
@@ -2,6 +2,8 @@ import { level0 } from "./levels/level0.js";
 import { level1 } from "./levels/level1.js";
 import { writeInfo } from "./info.js";
 
+const levelCount = document.getElementById("levelCount");
+
 export class Game {
     constructor(levelList) {
         this.levelList = [];
@@ -10,17 +12,18 @@ export class Game {
             lev.game = this;
             lev.index = this.levelList.length;
         }
-        this.currentLevelIndex = 0;
+        this.currentLevelIndex = 1;
     }
 
     get currentLevel() {
-        return this.levelList[this.currentLevelIndex];
+        return this.levelList[this.currentLevelIndex - 1];
     }
 
     start() {
         if (this.levelList.length === 0) return;
         this.currentLevel.drawObjects();
         this.currentLevel.addControls();
+        levelCount.innerText = "Level " + this.currentLevelIndex;
         writeInfo(
             "Press 'Space' to start or pause the game.<br>" +
                 "Press 'r' to restart the level."
@@ -29,10 +32,11 @@ export class Game {
 
     switchToNextLevel() {
         this.currentLevelIndex++;
-        if (this.currentLevelIndex >= this.levelList.length) {
+        if (this.currentLevelIndex > this.levelList.length) {
             writeInfo("You won the game!");
             return;
         }
+        levelCount.innerText = "Level " + this.currentLevelIndex;
         this.currentLevel.addControls();
         this.currentLevel.start();
     }
